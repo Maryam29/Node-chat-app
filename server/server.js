887928f9f60ab85265f7832fe0deaaacf38fcdda
maryam29 <maryam.dairkee@gmail.com>
2017-11-23 11:51:41 +0530

    First Commit

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+var app = express();
+const path = require('path');
+const publicPath = path.join(__dirname,'../public');
+const port = process.env.PORT || 3000;
+// console.log(__dirname+'/../public');
+// console.log(publicPath);
+app.use(express.static(publicPath));
+
+app.listen(port,()=>{
+	console.log('Started on port '+port);
+})
