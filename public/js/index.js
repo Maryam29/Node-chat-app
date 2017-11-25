742c77bf072742fb57e6eb385e47247cc6c5017f
maryam29 <maryam.dairkee@gmail.com>
2017-11-25 11:37:44 +0530

    Added auto scrolling

--- public/js/index.js
+++ public/js/index.js
@@ -1,66 +1,84 @@
-var socket = io(); // method available to socket.io.js file, we're initiating the request we're making a request from the client the the server to open up a web socket and keep that connection open. This handles whenever server is connected or disconnected
-// Arrow function may not work in IE or Safari , it may work well in chrome, so avoid using that.
-socket.on('connect',function(){
-console.log("Connected to server");
+var socket = io();
+
+function scrollToBottom () {
+  // Selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child')
+  // Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight(); //prev() moves to previous height
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
+socket.on('connect', function () {
+  console.log('Connected to server');
 });
 
-socket.on('newMessage',function(message){
-	var formattedTime = moment(message.createdAt).format('h:mm a');
-	var li = jQuery('<li></li>');
-	li.text(`${message.from} ${formattedTime}:${message.text}`);
-	jQuery('#messages').append(li);
-	//console.log(message);
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
 });
 
-socket.on('newLocationMessage',function(message){
-	var formattedTime = moment(message.createdAt).format('h:mm a');
-	var li = jQuery('<li></li>');
-	var a = jQuery(`<a target="_blank">My Current Location</a>`);
-	a.attr('href', message.url);
-	li.text(`${message.from} ${formattedTime}:`);
-	li.append(a);
-	jQuery('#messages').append(li);
-	//console.log(message);
+socket.on('newMessage', function (message) {
+  var formattedTime = moment(message.createdAt).format('h:mm a');
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
+
+  jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
-socket.on('disconnect',function(){
-console.log("disconnected from server");
+socket.on('newLocationMessage', function (message) {
+  var formattedTime = moment(message.createdAt).format('h:mm a');
+  var template = jQuery('#location-message-template').html();
+  var html = Mustache.render(template, {
+    from: message.from,
+    url: message.url,
+    createdAt: formattedTime
+  });
+
+  jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
-// socket.emit('createMessage',{
-	// from:"mike@gmal.com",
-	// text:"Hi! My Name is Shoeb"
-// },function(response){
-	// console.log(response)
-// });
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault();
 
-jQuery('#message-form').on('submit',function(e){
-	e.preventDefault();
-	var messageTextBox = jQuery("[name=message]");
-	socket.emit('createMessage',{
-	from:"User",
-	text:messageTextBox.val()
-},function(response){
-	messageTextBox.val("")
-});
+  var messageTextbox = jQuery('[name=message]');
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: messageTextbox.val()
+  }, function () {
+    messageTextbox.val('')
+  });
 });
 
 var locationButton = jQuery('#send-location');
-locationButton.on('click', function(){
-	if(!navigator.geolocation){
-		return alert('Geolocation not supported by your browser.');
-	}
-	locationButton.attr('disabled','disabled').text('Sending location...');
-	navigator.geolocation.getCurrentPosition(function(position){
-		//console.log(position);
-		socket.emit('createLocationMessage',{
-			longitude: position.coords.longitude,
-			latitude: position.coords.latitude
-		},function(response){
-	locationButton.removeAttr('disabled').text('Send location');
+locationButton.on('click', function () {
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser.');
+  }
+
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+
+  navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send location');
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  }, function () {
+    locationButton.removeAttr('disabled').text('Send location');
+    alert('Unable to fetch location.');
+  });
 });
-	},function(){
-		locationButton.removeAttr('disabled').text('Send location');
-		alert('Unable to fetch location.')
-	})
-})
